fae54631f22c9f029074f4596f4b29d83ad3863d
Joe Turner <joe@oampo.co.uk>
2016-05-20 13:00:28 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,53 @@
+/**
+ * Check whether a string starts with banana
+ */
+exports.startsWithBanana = function(str) {
+    return false;
+};
+
+/**
+ * Check whether a string ends with banana
+ */
+exports.endsWithBanana = function(str) {
+    return false;
+};
+
+/**
+ * Check whether a string is either banana, or bananas
+ */
+exports.isBananas = function(str) {
+    return false;
+};
+
+/**
+ * Check whether a string is banana with an optionally lengthened final
+ * vowel sound (bananaaaaaaaaaaaaaaa)
+ */
+exports.isBananaaa = function(str) {
+    return false;
+};
+
+/**
+ * Check whether a string is banana with optional additional nas (banananana)
+ */
+exports.isBanananana = function(str) {
+    return false;
+};
+
+/**
+ * Check whether a string is banana, but allow other alphabetical starting
+ * characters (zanana, lanana, etc).
+ */
+exports.isAlphanana = function(str) {
+    return false;
+};
+
+/**
+ * Check whether a string is a sequence of the word banana, separated by
+ * whitespace.  No starting or trailing whitespace is allowed.
+ */
+exports.isBananaSplit = function(str) {
+    return false;
+};
+
+
